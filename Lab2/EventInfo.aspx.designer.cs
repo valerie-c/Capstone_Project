0880cc4bcd994f2d08d264a28b3c6b2f5b2a057e
Valerie Chang <zhang5zx@dukes.jmu.edu>
2020-10-26 01:08:08 -0400

    Rest of the changes

--- Lab2/EventInfo.aspx.designer.cs
+++ Lab2/EventInfo.aspx.designer.cs
@@ -49,5 +49,23 @@ namespace Lab2
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView GridView3;
+
+        /// <summary>
+        /// GridView4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView4;
+
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
